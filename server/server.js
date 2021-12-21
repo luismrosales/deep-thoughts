3448336ba747cd934b65cc60bfd85e98986d7157
Luis Rosales <luismrosales2@gmail.com>
2021-12-20 23:04:29 -0500

    added mutations

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
@@ -13,6 +14,7 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: authMiddleware,
   });
 
   await server.start();
